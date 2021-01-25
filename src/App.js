8461cc2c76db828f86d4f6a4170337d0b4fc1638
edox61 <63751822+edox61@users.noreply.github.com>
2021-01-25 18:27:41 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import ResizeApp from "./ResizeApp";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <ResizeApp />
+    </div>
+  );
+};
+export default App;
---
+++ src/ResizeApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResizeApp = () => {
+  return (
+    <div>
+      <h1>ResizeApp</h1>
+    </div>
+  );
+};
+
+export default ResizeApp;
